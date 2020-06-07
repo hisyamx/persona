2e2e848db34aa0ad9c4dffaacc8c1d96f4882411
Khusnul Hisyam <khsyaam62@gmail.com>
2020-06-07 21:48:54 +0700

    change description

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,14 @@
 module.exports = {
 
-    author: "@konstantinmuenster",
-    siteTitle: "Gatsby Starter Portfolio Minimal",
-    siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
-    siteDescription: "A modern one-page portfolio with a clean yet expressive design.",
-    siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+    author: "@hisyamk",
+    siteTitle: "Khusnul Hisyam",
+    siteShortTitle: "Hisyam", // Used as logo text in header, footer, and splash screen
+    siteDescription: "Let's talk about interface design and life.",
+    siteUrl: "https://hisyamk.netlify.app/",
     siteLanguage: "en_US",
     siteIcon: "src/content/favicon.png", // Relative to gatsby-config file
 
-    splashScreen: false, // Set this to true if you want to use the splash screen
+    splashScreen: true, // Set this to true if you want to use the splash screen
 
     // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
     // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
@@ -20,35 +20,27 @@ module.exports = {
     socialMedia: [
         {
             name: "Linkedin",
-            url: "https://www.linkedin.com/in/konstantin-muenster/"
+            url: "https://www.linkedin.com/in/hisyamk/"
         },
         {
             name: "Medium",
-            url: "https://medium.com/@konstantin.muenster"
+            url: "https://medium.com/@hisyamk"
         },
         {
             name: "Github",
-            url: "https://github.com/konstantinmuenster/gatsby-starter-portfolio-minimal"
+            url: "https://github.com/khsyaam"
         },
         {
             name: "Behance",
-            url: "https://www.behance.net/konstanmnster"
+            url: "https://www.behance.net/hisyamk_"
         },
     ],
   
     navLinks: {
         menu: [
             {
-                name: "Articles",
-                url: "/#articles",
-            },
-            {
                 name: "About Me",
                 url: "/#about",
-            },
-            {
-                name: "Features",
-                url: "/#projects",
             },
         ],
         button: {
@@ -62,9 +54,5 @@ module.exports = {
             name: "Privacy",
             url: "/privacy"
         },
-        {
-            name: "Imprint",
-            url: "/imprint"
-        }
     ]
 }
